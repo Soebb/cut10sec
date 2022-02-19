426a14f47feeea5f75332e50b8f42a52f2e106ef
Soebb <87156166+Soebb@users.noreply.github.com>
2022-02-19 18:48:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ BOT_NAME = "cuter-RoBot"
 
 # system path to geckodriver.exe (Firefox driver)
 driver_path = r"C:\Program Files\Mozilla Firefox\geckodriver.exe"
-Firefox_version = "97.0.1"
+Firefox_version = ""
 
 # namasha upload option
 upload2namasha_option = True
